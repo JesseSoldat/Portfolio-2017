3757f1156567c6ef0820366eb8323078f0f1968c
jesse <jsoldat@hotmail.com>
2017-06-07 16:22:03 -0400

    added navigationEnd to scroll to the top during route changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,52 +1,22 @@
-import { Component } from '@angular/core';
-import { trigger, state, style, animate, transition, keyframes } from '@angular/animations';
+import { Component, OnInit } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
+import 'rxjs/Rx';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  animations: [
-    trigger('focusPanel', [
-        state('inactive', style({
-            transform: 'scale(0)',
-            backgroundColor: '#eee'
-        })),
-        state('active', style({
-            transform: 'scale(1.1)',
-            backgroundColor: '#cfd8dc',
-            borderRadius: '5px',
-            border: '1px solid black',
-            boxShadow: '10px 10px 15px black'
-        })),
-        transition('inactive => active', animate('1000ms ease-in')),
-        transition('active => inactive', animate('1000ms ease-out'))
-    ]),
-    trigger('myAwesomeAnimation', [
-        state('small', style({
-            transform: 'scale(1)',
-        })),
-        state('large', style({
-            transform: 'scale(1.9)',
-        })),
-        // transition('small <=> large', animate('1000ms ease-in')),
-    //     transition('small <=> large', animate('1500ms linear', style({
-    //       transform: 'translateY(200px)'
-    //     }))),
-    // 
-      transition('small <=> large', animate('1500ms linear', keyframes([
-        style({opacity: 0, transform: 'translateY(-75%)', offset: 0}),
-        style({opacity: 1, transform: 'translateY(35px)',  offset: 0.5}),
-        style({opacity: 1, transform: 'translateY(0)',     offset: 1.0})
-      ]))),
-    ]),
-   ]
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
 	contactType: string = '';
 
-  // state: string = 'inactive';
-  state: string = 'small';
+  constructor(public router: Router) {}
 
+  ngOnInit() {
+     this.router.events.filter(event => event instanceof NavigationEnd).subscribe(() => {
+      window.scrollTo(0, 0);
+    });
+  }
 
   contact(contact: string) {
   	this.contactType = contact;
@@ -56,11 +26,5 @@ export class AppComponent {
   	this.contactType = '';
   }
 
-  toggleMove() {
-    // this.state = (this.state === 'inactive' ? 'active' : 'inactive');
-  }
 
-  animateMe() {
-    this.state = (this.state === 'small' ? 'large' : 'small');
-  }
 }
