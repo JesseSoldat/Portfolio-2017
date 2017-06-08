9bac329de58203771e9568974fe3b464b342bd6d
jesse <jsoldat@hotmail.com>
2017-06-08 14:22:19 -0400

    about page is mobile ready

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -9,10 +9,16 @@ import { routerTransition } from '../../animations/slide-in.animation';
  	// host: { '[@routerTransition]': '' }
 })
 export class HomeComponent implements OnInit {
+	toggleImg: string = 'jessePic';
+	toggleMsg: string = 'Show Code';
 
   constructor() { }
 
   ngOnInit() {
+  }
+  onToggleImg() {
+  	this.toggleImg = this.toggleImg === 'jesseCode' ? 'jessePic' : 'jesseCode';
+  	this.toggleMsg = this.toggleMsg === 'Show Code' ? 'show Picture' : 'Show Code';
   }
 
 }
