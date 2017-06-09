86aeb57133c9e8457a68cc8fcc4761d5d0918288
jesse <jsoldat@hotmail.com>
2017-06-09 14:32:22 -0400

    worked on responsive design for work page

--- src/app/components/work/work-details/work-details.component.ts
+++ src/app/components/work/work-details/work-details.component.ts
@@ -18,21 +18,21 @@ export class WorkDetailsComponent implements OnInit {
   			"img": "fivewords.JPG",
   			"url": "http://five-words.surge.sh",
         "github": "https://github.com/5-Words",
-  			"description": "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+  			"description": "Five Words is an app for finding friends based on your most import words."
   		},
   		"around-world": {
   			"title": "Around the World",
   			"img": "aroundworld.jpg",
   			"url": "http://lonely-opinion.surge.sh",
         "github": "https://github.com/JesseSoldat/Around-The-World-NG2-Firebase",
-  			"description": "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+  			"description": "Around the world lets you map the places you lived or traveled to and find new friends by searching for others that share these special places."
   		},
   		"movie-buddy": {
   			"title": "Movie Buddy",
   			"img": "fivewords.JPG",
   			"url": "http://five-words.surge.sh",
         "github": "https://github.com/JesseSoldat/Movie-Buddy-NG4---Firebase",
-  			"description": "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+  			"description": "Have you ever had a hard time finding a good movie? Movie Buddy is an app that lets you build a list of your favorite movies and explore other user's lists of movies and easily select"
   		}
   	}
 
