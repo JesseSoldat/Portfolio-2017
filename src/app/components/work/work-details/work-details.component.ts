d7b1c7e1f0960db4e5c7be0a03a3331590948814
jesse <jsoldat@hotmail.com>
2017-06-13 11:53:57 -0400

    changed links to project

--- src/app/components/work/work-details/work-details.component.ts
+++ src/app/components/work/work-details/work-details.component.ts
@@ -23,14 +23,14 @@ export class WorkDetailsComponent implements OnInit {
   		"around-world": {
   			"title": "Around the World",
   			"img": "aroundworld.jpg",
-  			"url": "http://abounding-parcel.surge.sh",
+  			"url": "https://world-view.surge.sh",
         "github": "https://github.com/JesseSoldat/Around-The-World-NG2-Firebase",
   			"description": "Around the world lets you map the places you lived or traveled to and find new friends by searching for others that share these special places.  How big is your world?  Not traveled much?  No worries.  Start with finding stories near your locale.  Want to branch out?  Then just increase the distance of your search as you explore the world one story at a time."
   		},
   		"movie-buddy": {
   			"title": "Movie Buddy",
   			"img": "movie-buddy.jpg",
-  			"url": "http://marvelous-size.surge.sh",
+  			"url": "https://movie-buddy.surge.sh",
         "github": "https://github.com/JesseSoldat/Movie-Buddy-NG4---Firebase",
   			"description": "Have you ever had a hard time finding a good movie? If so Movie Buddy can help.  Just begin typing the name and Movie Buddy will start returning results from an extensive database full of movies.  Movie Buddy also offers a powerful search mechanism for displaying other users that also share many of your favorite movies.  From there you can view the movies that they like that are not yet on your list and easily add  them to your collection."
   		}
